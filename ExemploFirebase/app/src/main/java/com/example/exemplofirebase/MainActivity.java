eaba228038ca4ded6038d7d418384724306073aa
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-10-08 19:42:46 -0300

    projeto final

---
+++ ExemploFirebase/app/src/main/java/com/example/exemplofirebase/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.exemplofirebase;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        db = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+         db.collection("users").add(user)
+                         .addOnSuccessListener(command -> {
+                             Toast.makeText(this, "Inseriu com sucesso!",
+                                     Toast.LENGTH_SHORT).show();
+                         })
+                        .addOnFailureListener(command -> {
+                            Toast.makeText(this, "Erro ao inserir!",
+                                    Toast.LENGTH_SHORT).show();
+                        });
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
